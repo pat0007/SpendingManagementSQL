7f2945ab4ba33b80f7b6310136564ba1b593ce86
Pierce <pat0007@auburn.edu>
2020-04-10 17:52:02 -0500

    added functionality for adding elements to db.

--- app/src/main/java/com/pat0007/spendingmanagementsql/MainActivity.java
+++ app/src/main/java/com/pat0007/spendingmanagementsql/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -20,11 +22,59 @@ public class MainActivity extends AppCompatActivity {
     String history;
     TextView header;
 
+    private View.OnClickListener listen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.add_btn:
+                    add_btn_clicked();
+                    break;
+                case R.id.minus_btn:
+                    minus_btn_clicked();
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         myDB = new DatabaseHelper(this);
+        header = findViewById(R.id.header);
+        date = findViewById(R.id.date);
+        amount = findViewById(R.id.amount);
+        purpose = findViewById(R.id.purpose);
+        add_btn = findViewById(R.id.add_btn);
+        minus_btn = findViewById(R.id.minus_btn);
+        applicationContext = this;
+
+        add_btn.setOnClickListener(listen);
+        minus_btn.setOnClickListener(listen);
+    }
+
+    private void add_btn_clicked() {
+        String dateText = date.getText().toString();
+        String amountText = amount.getText().toString();
+        String purposeText = purpose.getText().toString();
+
+        BigDecimal newBalance = new BigDecimal(amountText);
+
+        balance = balance.add(newBalance);
+        header.setText("Current Balance: $" + balance);
+
+        myDB.insertData(dateText, amountText, purposeText);
+    }
+
+    private void minus_btn_clicked() {
+        String dateText = date.getText().toString();
+        String amountText = amount.getText().toString();
+        String purposeText = purpose.getText().toString();
+
+        BigDecimal newBalance = new BigDecimal(amountText);
+
+        balance = balance.subtract(newBalance);
+        header.setText("Current Balance: $" + balance);
     }
 }
